e3c1de8a7590a59e652e365b4b7ef53895840aa4
LautaroAntriolo <lautaroantriololuis@gmail.com>
2023-03-08 18:43:13 -0300

    organización en funciones

--- Toot.py
+++ Toot.py
@@ -10,13 +10,32 @@ masto = Mastodon(
     api_base_url = "https://masto.es"
 )
 
-async def post_video(mje,media):
+async def post_video(mje,media,Sensitive=False, formato='mp4',visibilidad='public'):
     with open(f'{media}', 'rb') as video_file:
         video_bytes = video_file.read()
-    media_dict = masto.media_post(media_file=video_bytes, mime_type='video/mp4')
+    media_dict = masto.media_post(media_file=video_bytes, mime_type=f'video/{formato}')
     media_id = media_dict['id']
     await asyncio.sleep(5) # espera 10 segundos antes de publicar
-    await masto.status_post(status=f'{mje}', media_ids=[media_id], sensitive=False, visibility='public')
+    await masto.status_post(status=f'{mje}', media_ids=[media_id], sensitive=Sensitive, visibility=f'{visibilidad}')
 
-asyncio.run(post_video('¡Gracias a Dios es Viernes!', 'video/viernes/graciasADiosEsViernes.mp4'))
-# asdas
+
+async def post_imagen(mje,media,Sensitive=False, formato='png',visibilidad='public'):
+    media_dict = masto.media_post(media_file=media, mime_type=f'imagen/{formato}')
+    media_id = media_dict['id']
+    # await asyncio.sleep(5) # espera 10 segundos antes de publicar
+    masto.status_post(status=f'{mje}', media_ids=[media_id], sensitive=Sensitive, visibility=visibilidad)
+
+def video(mje,media,*args, Sensitive=False, formato='mp4',visibilidad='public'):
+    numerales = ''
+    for arg in args:
+        numerales += f'#{arg} '
+    asyncio.run(post_video(f'''{mje}
+{numerales}''', f'{media}'))
+
+def imagen(mje,media,*args, Sensitive=False, formato='png',visibilidad='public'):
+    numerales = ''
+    for arg in args:
+        numerales += f'#{arg} '
+    
+    asyncio.run(post_imagen(f'''{mje}
+{numerales}''', f'{media}'))
---
+++ main.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import Toot
+
+def funcionViernes():
+    Toot.video('Gracias a Dios es viernes','video/viernes/graciasADiosEsViernes.mp4', 'viernes','viernesSanto','otroHastag')  
+
+def funcionDiaria():
+    Toot.imagen("Vamos a ver como se publica esta imagen", 'img/imagenHormigas1.png', 'hormigas','IA','mdjurnie')
+# funcionViernes()
+
+funcionDiaria()
